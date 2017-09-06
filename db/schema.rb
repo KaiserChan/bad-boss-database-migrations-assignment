d34b6eecfe5c426dc3c0b782a11e25fd92881915
KaiserChan <chankaiserkk@gmail.com>
2017-09-06 00:00:57 -0400

    updated badboss - created name change migration, change party guest table name to widgets:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906034804) do
+ActiveRecord::Schema.define(version: 20170906035715) do
 
   create_table "locations", force: :cascade do |t|
     t.string "facility"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170906034804) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
